3f04b7509e991c05b813d2710e36f6abdfd0aff8
IAComprasArmandoPneus <glauco@armandopneus.com.br>
2025-04-28 23:46:12 -0300

    Update getNFEntrada.js

--- docs/api/getNFEntrada.js
+++ docs/api/getNFEntrada.js
@@ -1,7 +1,7 @@
 // /api/getNFEntrada.js
 
 import { google } from 'googleapis';
-import { auth } from '../../utils/googleAuth';
+import { auth } from '../googleAuth';
 
 const CACHE_TTL_MS = 60 * 1000; // cache de 1 minuto
 let nfCache = { timestamp: 0, data: null };
